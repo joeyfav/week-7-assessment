f3e5339d6dbd6f466e80d716ce5c96074fd84f26
joeyfav <joefav161@gmail.com>
2023-11-12 12:24:29 +0100

    my first commit, adding javascript codes

---
+++ script.js
@@ -0,0 +1,26 @@
+class Animal {
+    constructor(species, sound) {
+      this.species = species;
+      this.sound = sound;
+    }
+  
+    makeSound() {
+      console.log(this.sound);
+    }
+  }
+  class Dog extends Animal {
+    constructor(species, sound, color) {
+      super(species, sound);
+      this.color = color;
+    }
+    makeSound() {
+      super.makeSound();
+      console.log(`Color: ${this.color}`);
+    }
+  }
+  // here we create an instance of the Dog class
+  const dog = new Dog('Dog', 'Woof woof!', 'grey');
+  
+  // here, we make the dog make its sound
+  dog.makeSound();
+  
